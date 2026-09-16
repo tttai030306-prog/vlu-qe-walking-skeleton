72309b62be04c9f1d81ddc0e547500731c421485
tttai030306-prog <tttai030306@gmail.com>
2026-09-16 16:44:48 +0700

    Break login test intentionally

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function checkLogin(username, password) {
-  return username === 'admin' && password === '123';
+  return username === 'admin' && password === '1234';
 }
 
 if (typeof document !== 'undefined') {
