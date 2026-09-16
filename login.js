003ce870dd26a8863c6d504b0b2db402abb29d7d
tttai030306-prog <tttai030306@gmail.com>
2026-09-16 16:48:18 +0700

    Fix login password

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function checkLogin(username, password) {
-  return username === 'admin' && password === '1234';
+  return username === 'admin' && password === '123';
 }
 
 if (typeof document !== 'undefined') {
